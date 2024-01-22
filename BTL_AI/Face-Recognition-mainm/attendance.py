7435231d071f89fe190c99be489f932db260c796
DucL0c <tranducloc2010@gmail.com>
2024-01-22 18:49:36 +0700

    update

---
+++ BTL_AI/Face-Recognition-mainm/attendance.py
@@ -0,0 +1,97 @@
+import cv2
+import openpyxl
+import os
+import shutil
+
+class FaceRecognition:
+    def __init__(self, face_detect_path, excel_path):
+        self.video = cv2.VideoCapture(0)
+        self.path = os.path.dirname(__file__)
+        # file haarcascade
+        self.facedetect = cv2.CascadeClassifier(face_detect_path)
+        self.excel_path = excel_path 
+        # Thông qua phương pháp create(), một đối tượng recognizer mới được tạo ra và sẵn sàng để huấn luyện 
+        # và dự đoán khuôn mặt. Sau khi tạo đối tượng này, bạn có thể sử dụng nó để huấn luyện mô hình 
+        # với dữ liệu ảnh, đồng thời cung cấp một số tham số có thể điều chỉnh để tối ưu hóa quá trình nhận dạng.
+        self.recognizer = cv2.face_LBPHFaceRecognizer.create()
+        trainer_file = os.path.join(self.path, "trainer.yml")
+        try:
+            self.recognizer.read(trainer_file)
+        except Exception as e:
+            print("Cảnh báo: Xảy ra lỗi khi đọc tệp 'trainer.yml'")
+            raise Exception("Không thể tạo đối tượng vì lỗi đọc tệp 'trainer.yml'")
+        self.student_ids = self.read_ids_from_excel()
+
+    def read_ids_from_excel(self):
+        student_ids = {}
+        try:
+            workbook = openpyxl.load_workbook(os.path.join(self.path, "data1.xlsx"))
+            sheet = workbook.active
+            for row in sheet.iter_rows(min_row=2, values_only=True):
+                if len(row) >= 3:
+                    id, name, student_id = int(row[0]), row[1], row[2]
+                    student_ids[id] = student_id
+        except Exception as e:
+            print(f"An error occurred while reading the Excel file: {e}")
+
+        return student_ids
+
+    def run(self):
+        while True:
+
+            ret, frame = self.video.read()
+            if not ret:
+                print("Không thể đọc khung hình từ camera.")
+                break
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = self.facedetect.detectMultiScale(gray, 1.3, 5)
+            for x, y, w, h in faces:
+                serial, confident = self.recognizer.predict(gray[y : y + h, x : x + w])
+                if confident < 50:
+                    print(confident)
+                    id = self.student_ids.get(serial)
+                    cv2.putText(
+                        frame,
+                        f"{str(id).upper()}",
+                        (x - 100, y + h - 300),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        1,
+                        (0, 255, 0),
+                        2,
+                    )
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                    self.update_status_in_excel(serial)
+
+                else:
+                    cv2.putText(
+                        frame,
+                        "Unknown",
+                        (x, y + h - 300),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        1,
+                        (0, 0, 255),
+                        2,
+                    )
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.imshow("Face Recognition", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        self.video.release()
+        cv2.destroyAllWindows()
+
+    def update_status_in_excel(self, student_id):
+        wb = openpyxl.load_workbook(self.excel_path)
+        ws = wb.active
+        for row in ws.iter_rows(min_row=2, values_only=True):
+            if row[0] == student_id:
+                row_index = student_id
+                if row[-1] == "V":
+                    ws.cell(row=row_index, column=ws.max_column, value="X")
+            wb.save(self.excel_path)
+        self.create_backup()
+        
+    def create_backup(self):
+        if os.path.exists(self.excel_path):
+            backup_file = os.path.join(self.path, "backup_data1.xlsx")
+            shutil.copyfile(self.excel_path, backup_file)
---
+++ BTL_AI/Face-Recognition-mainm/register.py
@@ -0,0 +1,94 @@
+from tkinter import messagebox
+import cv2
+import os
+import numpy as np
+from PIL import Image
+import openpyxl
+
+
+class FaceRegister:
+    def __init__(self, data_path, recognizer_path, excel_path):
+        self.recognizer = cv2.face_LBPHFaceRecognizer.create()
+        # file anh
+        self.data_path = data_path 
+        #file train
+        self.recognizer_path = recognizer_path
+        self.excel_path = excel_path
+
+    def collect_data(self):
+        user_id = self.get_next_id()
+        video = cv2.VideoCapture(0)
+
+        facedetect = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        count = 0
+
+        while True:
+            ret, frame = video.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            faces = facedetect.detectMultiScale(gray, 1.3, 5)
+
+            for x, y, w, h in faces:
+                count += 1
+                cv2.imwrite(
+                    os.path.join(self.data_path, f"User.{user_id}.{count}.jpg"),
+                    gray[y : y + h, x : x + w],
+                )
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 50, 255), 2)
+
+            cv2.imshow("Frame", frame)
+
+            k = cv2.waitKey(1)
+            if k == ord("q"):
+                break
+            if count > 200:
+                break
+
+        video.release()
+        cv2.destroyAllWindows()
+        messagebox.showinfo("Thông báo", "Thu thập dữ liệu thành công.")
+
+    def train_model(self):
+        imagePaths = [
+            os.path.join(self.data_path, f) for f in os.listdir(self.data_path)
+        ]
+        faces = []
+        IDs = []
+
+        for imagePath in imagePaths:
+            # Image.open và chuyển đổi sang chế độ ảnh xám bằng phương thức convert("L"). 
+            # Sau đó, nó được chuyển đổi thành mảng numpy faceNp.
+            faceImg = Image.open(imagePath).convert("L")
+            # Ảnh xám sau đó được chuyển đổi thành mảng numpy với kiểu dữ liệu "uint8".
+            # Mỗi mảng này đại diện cho một khuôn mặt trong tập dữ liệu huấn luyện.
+            faceNp = np.array(faceImg, "uint8")
+            #  Đoạn mã này trích xuất ID người dùng từ tên tệp ảnh. 
+            # Đây giả định rằng tên tệp có định dạng "<tên>.<id>.jpg" hoặc tương tự.
+            user_id = int(os.path.split(imagePath)[-1].split(".")[1])
+
+            faces.append(faceNp)
+            IDs.append(user_id)
+
+        if len(IDs) > 0:
+            messagebox.showinfo("Thông báo", "Processing...")
+            self.recognizer.train(faces, np.array(IDs))
+            self.recognizer.save(self.recognizer_path)
+            messagebox.showinfo("Thông báo", "Huấn luyện hoàn tất.")
+        else:
+            messagebox.showerror("Lỗi", "Không tìm thấy dữ liệu huấn luyện.")
+
+    def save_data(self, name, student_id, dob, class_name):
+        wb = openpyxl.load_workbook(self.excel_path)
+        ws = wb.active
+        next_id = self.get_next_id()
+        ws.cell(row=next_id, column=1, value=next_id)
+        ws.cell(row=next_id, column=2, value=name)
+        ws.cell(row=next_id, column=3, value=student_id)
+        ws.cell(row=next_id, column=4, value=dob)
+        ws.cell(row=next_id, column=5, value=class_name)
+        wb.save(self.excel_path)
+
+    def get_next_id(self):
+        wb = openpyxl.load_workbook(self.excel_path)
+        ws = wb.active
+        return ws.max_row + 1
